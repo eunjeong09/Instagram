76cf7df4d0844303c2b91b94d67ec317ba03a5d0
won junho <wnsgh1993@gmail.com>
2022-06-01 17:04:59 +0900

    "vuex-persistedstate": "^4.1.0" 설치 및 vuex 적용

--- mypofolgram-front/src/store/index.js
+++ mypofolgram-front/src/store/index.js
@@ -2,6 +2,7 @@ import { createStore } from "vuex";
 import UserModule from './user'
 import AuthModule from './auth'
 import CommentModule from './comments'
+import createPersistedState from "vuex-persistedstate";
 // export default createStore({
 //     modules : {
 //         userInfo : UserModule,
@@ -14,6 +15,12 @@ const store = createStore ({
         userInfo : UserModule,
         auth : AuthModule,
         comment : CommentModule
-    }
+    },
+    plugins: [
+        createPersistedState({
+            //주목! : 여기에 쓴 모듈만 저장됩니다.
+            paths: ["userInfo","auth", "comment"],
+        }),
+    ],
 })
 export default store;
