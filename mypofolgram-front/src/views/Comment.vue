1271f0889327bab431a62a1a856fe55ee2d6327b
won junho <wnsgh1993@gmail.com>
2022-06-22 21:53:33 +0900

    comment 주석 제거

--- mypofolgram-front/src/views/Comment.vue
+++ mypofolgram-front/src/views/Comment.vue
@@ -60,36 +60,6 @@ export default {
     methods: {
         // api 호출시 파라미터로 게시글 아이디 전달
         getComments() {
-            let response = {
-                body: "여기완전 맛집",
-                nickName: "게시물 작성자",
-                userImgUrl: "/images/example.jpeg",
-                createDate: "2022/04/05",
-                hashtags: ["압구정 맛집", "압구정", "김치찌개 맛집"],
-                rows: [
-                    {
-                        id: 1, //댓글 아이디
-                        nickName: "eunjeong",
-                        userImgUrl: "/images/example.jpeg",
-                        comment: "인정인정",
-                        createDate: "2022/04/10",
-                    },
-                    {
-                        id: 2, //댓글 아이디
-                        nickName: "nick",
-                        userImgUrl: "/images/example.jpeg",
-                        comment: "@eunjeong",
-                        createDate: "2022/04/11",
-                    },
-                    {
-                        id: 3, //댓글 아이디
-                        nickName: "name",
-                        userImgUrl: "/images/example.jpeg",
-                        comment: "",
-                        createDate: "2022/04/1",
-                    },
-                ],
-            };
             http.get("/api/post/getCommentList", {params : {postId : "1"}})
             .then((response) => {
                 this.localCommentList = response.data.result
